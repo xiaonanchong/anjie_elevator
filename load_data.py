f34c99e79f97bacd939aa64a63f5751a6d7f52a7
xiaonanchong <xnchong@outlook.com>
2019-10-23 09:36:00 +0800

    Update load_data.py

--- load_data.py
+++ load_data.py
@@ -21,3 +21,31 @@ def load_and_slice_data(end, fname, rpm, target):
         a = int(x.shape[0]/slice_length)*slice_length
         x = np.array(x[:a]).reshape(-1, slice_length)
         y = np.array([[0,1] for i in range(x.shape[0])])
+    else:
+        x = None
+        for i in range(20):
+            x1 = data['X'+fname+'_DE_time'][int(random.random()*slice_length) :]
+            a1 = int(x1.shape[0]/slice_length)*slice_length
+            x1 = np.array(x1[:a1]).reshape(-1, slice_length)
+        
+            x2 = data['X'+fname+'_FE_time'][int(random.random()*slice_length) :]
+            a2 = int(x2.shape[0]/slice_length)*slice_length
+            x2 = np.array(x2[:a2]).reshape(-1, slice_length)
+            
+            x_ = np.concatenate((x1,x2), axis = 0)
+            if i == 0:
+                x = x_
+            else:
+                x = np.concatenate((x, x_), axis = 0)
+                
+            y = np.array([[1,0] for i in range(x.shape[0])])
+
+    print(x.shape)
+    print(y.shape)
+    return x, y 
+
+x1=load_and_slice_data('driverend', 105, 1797, [0,1])
+x2 =load_and_slice_data('fanend', 278, 1797, [0,1])
+load_and_slice_data('nornal', 97, 1797, [0,1])
+
+
